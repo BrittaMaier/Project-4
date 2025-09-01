d40a555ba4fe0c8cf44472f834218d26ea6c58ce
Britta <britta.3.maier@uni-konstanz.de>
2025-09-01 17:11:23 +0200

    Add: return of ColorForm including labels and inputs

--- src/Components/ColorForm.jsx
+++ src/Components/ColorForm.jsx
@@ -6,4 +6,25 @@ export default function ColorForm({ onAddColor }) {
     onAddColor(data);
     event.target.reset();
   }
+  return (
+    <form className="color-form" onSubmit={handleSubmit}>
+      <div className="color-form__field">
+        <label htmlFor="role">Role</label>
+        <input type="text" name="role" id="role" />
+        <label>
+          Hex
+          <input type="text" placeholder="#123456" />
+          <input type="color" placeholder="#123456" />
+        </label>
+        <label>
+          Contrast Text
+          <input type="text" placeholder="#ffff" />
+          <input type="color" placeholder="#ffff" />
+        </label>
+      </div>
+      <div className="color-form__button-wrapper">
+        <Button type="submit">ADD COLOR</Button>
+      </div>
+    </form>
+  );
 }
