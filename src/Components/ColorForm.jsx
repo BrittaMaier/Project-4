d39749db0c4f4b5fdfb54a959a93ab8520536c96
Britta <britta.3.maier@uni-konstanz.de>
2025-09-01 16:31:40 +0200

    Create: ColorForm component including handleSubmit function

---
+++ src/Components/ColorForm.jsx
@@ -0,0 +1,9 @@
+export default function ColorForm({ onAddColor }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    onAddColor(data);
+    event.target.reset();
+  }
+}
